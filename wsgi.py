db014094fe4bf30f2b63fda2ea689dddbb669662
SR <cryptofervid@gmail.com>
2022-08-22 02:21:37 +1000

    Added flash support

--- wsgi.py
+++ wsgi.py
@@ -4,7 +4,7 @@ import os
 
 load_dotenv()
 SECRET_KEY = os.getenv('SECRET_KEY')
+app.secret_key = os.getenv('SECRET_KEY')
 
 if __name__ == '__main__':
-    app.secret_key = os.getenv('SECRET_KEY')
     app.run()
