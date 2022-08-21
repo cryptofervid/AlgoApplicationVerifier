a5c3a9abc35fa718b6e8778757b603db176a199c
SR <cryptofervid@gmail.com>
2022-08-22 01:41:19 +1000

    Added flash support

--- wsgi.py
+++ wsgi.py
@@ -1,10 +1,9 @@
 from app import app
 from dotenv import load_dotenv
-import os
+import secrets
 
 load_dotenv()
 
 if __name__ == '__main__':
-    SECRET_KEY = os.getenv('SECRET_KEY')
-    app.config['SECRET_KEY'] = SECRET_KEY
+    app.secret_key = secrets.token_hex(24)
     app.run()
