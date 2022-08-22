5e4f80e842b590c4ce86110f622a7ae47e4f39e9
SR <cryptofervid@gmail.com>
2022-08-22 17:12:29 +1000

    Updated tests

--- tests/test_pytealhelper.py
+++ tests/test_pytealhelper.py
@@ -4,22 +4,20 @@ from pytealhelper import PyTealHelper
 
 def test_extract_code():
 
-    expected_value = """
-    #pragma version 2
-    byte "reserve"
-    byte "reserve"
-    app_global_get
-    int 0
-    byte "balance"
-    app_local_get
-    +
-    app_global_put
-    int 1
-    return
-    """
+    expected_value = """#pragma version 2
+byte "reserve"
+byte "reserve"
+app_global_get
+int 0
+byte "balance"
+app_local_get
++
+app_global_put
+int 1
+return"""
 
-    pytealgithub = GithubCodeRepository('https://github.com/algorand/pyteal/blob/master/examples/application/asset.py')
+    pytealgithub = GithubCodeRepository('https://github.com/cryptofervid/AlgoApplicationVerifier/blob/main/tests/resources/pyteal_example.py')
     pytealhelper = PyTealHelper(pytealgithub.get_raw_code(), 'approval_program', pytealgithub.get_raw_code(), 'clear_state_program', 2)
-    print(pytealhelper.get_approval_teal())
+    clear_state_teal = pytealhelper.get_clear_state_teal()
 
-    assert
+    assert clear_state_teal == expected_value
