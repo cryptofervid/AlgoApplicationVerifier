6ff930b75af548eb79a0f9be834c6e4073d761a0
SR <cryptofervid@gmail.com>
2022-08-22 16:50:38 +1000

    Updated tests

--- tests/test_algohelper.py
+++ tests/test_algohelper.py
@@ -1,18 +1,18 @@
 from algohelper import AlgoTealHelper
-from githubrepo import GithubCodeRepository
 
 
 def test_download_bytecode_downloads_code():
-    coderepo = GithubCodeRepository(
-        'https://github.com/tinymanorg/tinyman-contracts-v1/blob/main/contracts/validator_approval.teal')
-    code = coderepo.get_raw_code()
+
+    teal_code = """
+    #pragma version 2
+    int 1
+    return
+    """
+
+    expected_bytecode = "AiABASJD"
 
     algo_teal_helper = AlgoTealHelper(552635992)
     algo_teal_helper.download_teal_bytecode()
-    generated_bytecode = algo_teal_helper.teal_to_bytecode(code)
-    print(generated_bytecode)
+    generated_bytecode = algo_teal_helper.teal_to_bytecode(teal_code)
 
-    if generated_bytecode == algo_teal_helper.approval:
-        print("Verified code with commit-id: " + coderepo.commit_id)
-    else:
-        print("Not verified")
+    assert generated_bytecode == expected_bytecode
