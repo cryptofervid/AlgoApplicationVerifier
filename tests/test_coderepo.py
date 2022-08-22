5b3f19c915dcdbb473253d0576c05676fc5fa2e9
SR <cryptofervid@gmail.com>
2022-08-22 16:56:31 +1000

    Updated tests

--- tests/test_coderepo.py
+++ tests/test_coderepo.py
@@ -5,9 +5,9 @@ def test_getrawcode_returns_rawcode():
 
     expected_value = "This is a test file for testing repository code."
 
-    coderepo = GithubCodeRepository('https://github.com/tinymanorg/tinyman-contracts-v1/blob/13acadd1a619d0fcafadd6f6c489a906bf347484/contracts/validator_approval.teal')
-    #"https://raw.githubusercontent.com/sumantrana/StudentRestAPI/5b2365155f2c313d51e9cb006df1cbc6c5ecbd2f/src/test/java/com/sumant/rest/studentrest/CourseServiceTests.java"
+    coderepo = GithubCodeRepository('https://github.com/cryptofervid/AlgoApplicationVerifier/blob/main/tests/resources/test.txt')
     code = coderepo.get_raw_code()
-    print(code)
+
+    assert code == expected_value
 
 
--- tests/test_pytealhelper.py
+++ tests/test_pytealhelper.py
@@ -3,6 +3,23 @@ from pytealhelper import PyTealHelper
 
 
 def test_extract_code():
+
+    expected_value = """
+    #pragma version 2
+    byte "reserve"
+    byte "reserve"
+    app_global_get
+    int 0
+    byte "balance"
+    app_local_get
+    +
+    app_global_put
+    int 1
+    return
+    """
+
     pytealgithub = GithubCodeRepository('https://github.com/algorand/pyteal/blob/master/examples/application/asset.py')
-    pytealhelper = PyTealHelper(pytealgithub.get_raw_code(), 'approval_program', '', '', 2)
-    pytealhelper.get_approval_code()
+    pytealhelper = PyTealHelper(pytealgithub.get_raw_code(), 'clear_state_program', '', '', 2)
+    print(pytealhelper.get_approval_teal())
+
+    assert 
